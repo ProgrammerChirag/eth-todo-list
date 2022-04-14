546eb3595ed8a8d3957a285ad9602c877739e126
Chirag Juneja <junejachirag020@gmail.com>
2022-04-15 02:12:39 +0530

    project setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList {
+    /*
+    * this is state variable inside blockchain it shows state of a blockchain..
+    */
+    uint public taskCount = 0; 
+
+}
